9541fade81a34574709fe5d1adaee2391dd80799
siva <sivasank999@gmail.com>
2023-09-04 22:19:42 +0530

    server

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
@@ -18,14 +16,25 @@ const db = mysql.createConnection(urlDB);
 app.get('/api/getmenu',(req,res)=>{
     const sql = "select * from menu";
     db.query(sql,(err,result)=>{
-        return res.json(result)
+      if(err){
+        res.json("error")
+      }
+      else{
+        return res.json("data received")
+      }
     })
 })
 
 app.get('/api/getmenuitems',(req,res)=>{
     const sql = "select * from menuitems";
+
     db.query(sql,(err,result)=>{
+      if(err){
+        res.json("error")
+      }
+      else{
         return res.json(result)
+      }
     })
 })
 
@@ -35,7 +44,7 @@ app.get('/api/getmsg',(req,res)=>{
     })
 })
 
-const port = process.env.PORT || 6752;
+const port = process.env.MYSQLPORT || 6752;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
