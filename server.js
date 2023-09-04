7897947bceb97486edc80c712d5faa0a1428c328
siva <sivasank999@gmail.com>
2023-09-04 21:47:13 +0530

    server

--- server.js
+++ server.js
@@ -11,12 +11,9 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
-});
+const urlDB = `mysql://${process.env}:${process.env}@${process.env}:${process.env}/${process.env}`;
+
+const db = mysql.createConnection(urlDB);
 
 app.get('/getexample',(req,res)=>{
     const sql = "select * from menu";
